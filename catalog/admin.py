74e77c83897e984b1c8476af0927918372062f80
Goro Collins <gorocollins@gmail.com>
2023-07-14 16:12:19 +0300

    admin and views

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,10 +1,29 @@
 from django.contrib import admin
-from .models import Author, Genre, Book, BookInstance
+from .models import Author, Genre, Book, BookInstance, Language
 
 # Register your models here.
+class BooksInline(admin.StackedInline):
+    model = Book
+    extra = 0
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')] # fields that will be shown on the form
+    inlines = [BooksInline]
 
+admin.site.register(Author, AuthorAdmin)
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'display_genre']
+    inlines = [BooksInstanceInline]
+admin.site.register(Book, BookAdmin)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ['status', 'due_back']
+    fieldsets = ((None, {'fields':('id', 'book', 'imprint')}),
+                 ('Availability',{'fields':('due_back', 'status')}),)
 
-admin.site.register(Book)
-admin.site.register(Author)
+admin.site.register(BookInstance, BookInstanceAdmin)
+
+admin.site.register(Language)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
--- catalog/models.py
+++ catalog/models.py
@@ -27,9 +27,14 @@ class Book(models.Model):
 
     def __str__(self) -> str:
         return self.title
-    
+    # Creates view on site button
     def get_absolute_url(self):
         return reverse('book-detail', args=[str(self.id)])
+    # required for the implementation of display_genre
+    def display_genre(self):
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
 
 class BookInstance(models.Model):
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
